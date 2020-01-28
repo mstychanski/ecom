40bfae287cb6119a9fbb044f099d16822a52e415
Marcin Piskorz <marcin.piskorz@sollers.eu>
2020-01-28 22:08:50 +0100

    First endpoint added

---
+++ src/main/java/ecom/pac/EcomApplication.java
@@ -0,0 +1,13 @@
+package ecom.pac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomApplication.class, args);
+	}
+
+}
